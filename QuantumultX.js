112e83e4ba8fa14c49cc6c4388d76c8eed704ccc
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2023-02-12 04:33:42 +0800

    Create QuantumultX.js

---
+++ QuantumultX.js
@@ -0,0 +1,39 @@
+/************
+
+Quantumultx
+
+#脚本作者：伟人
+#联系作者QQ:
+#最后更新时间：2023-02-12
+#问题反馈：QQ+ 小号:
+#作者忠告：所有脚本仅限内部测试、学习、开发。禁止用于商业用途！脚本全部由 伟人 制作维护更新。改名搬砖，行骗都属于不道德行为！
+#QQ会员群：PY作者
+#TG反馈群：https://t.me/WeiRenOvO
+#TG频道群：https://t.me/WeiRenQAQ
+#
+注意 : 添加后需要勾选->资源解析器<-否则无法更新。
+
+
+*************/
+
+
+
+[rewrite_remote]
+
+
+
+
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/kw.js, tag=by伟人-酷我音乐SVIP, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/zhihu6.js, tag=by伟人-知乎VIP, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/FD.js, tag=by伟人-樊登读书VIP, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/beile.js, tag=by伟人-贝乐虎启蒙解锁, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/yunt.js, tag=by伟人-云听VIP, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/wyun.js, tag=by伟人-网易云音乐VIP, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/XTJYQY.js, tag=by伟人-剪映+醒图+轻言相机VIP, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/xmet.js, tag=by伟人-喜马拉雅儿童版VIP, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/ximalaya.js, tag=by伟人-喜马拉雅VIP, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/weisbj.js, tag=by伟人-微商笔记VIP, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Free1/main/Lsp.conf, tag=by伟人-LSP脚本, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/BLBLHD.js, tag=by伟人-哔哩哔哩解锁画质, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/wufan.js, tag=by伟人-解锁-悟饭趣玩|游戏厅|酷玩盒|掌悦, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/keep.js, tag=by伟人-解锁keep课程解锁+, update-interval=172800, opt-parser=true, enabled=true
