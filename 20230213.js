f0572e244fe358b69f855ae7104890bd630621ca
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2023-02-13 19:54:55 +0800

    Delete 20230213.js

--- 20230213.js
+++
@@ -1,8 +0,0 @@
-/***
-喜马拉雅2号线路
-
-***/
-
-[rewrite_remote]
-
-https://raw.githubusercontent.com/WeiRen0/Scripts/main/ximalaya02.js, tag=by伟人-喜马拉雅2号线路, update-interval=172800, opt-parser=true, enabled=true
