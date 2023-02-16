6d16ac3f3ef8d7e97998b0106af0340ca5e22042
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2023-02-16 11:46:34 +0800

    Update 20230216.js

--- 20230216.js
+++ 20230216.js
@@ -2,4 +2,4 @@
 
 [rewrite_remote]
 
-https://raw.githubusercontent.com/WeiRen0/Scripts/main/ximalaya03.js, tag=by伟人-喜马拉雅2号线路, update-interval=172800, opt-parser=true, enabled=true
+https://raw.githubusercontent.com/WeiRen0/Scripts/main/ximalaya03.js, tag=by伟人-喜马拉雅3号线路, update-interval=172800, opt-parser=true, enabled=true
